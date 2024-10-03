e89ee36ed902796d312e540f17062c13c3ec2991
GilangWidi <gilangwidir@gmail.com>
2024-10-03 18:31:04 +0700

    Add files via upload

---
+++ data/KantorDesa_2.js
@@ -0,0 +1,13 @@
+var json_KantorDesa_2 = {
+"type": "FeatureCollection",
+"name": "KantorDesa_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah Talang Sumut", "X": 472231.43, "Y": 9670099.32 }, "geometry": { "type": "Point", "coordinates": [ 104.750114962434267, -2.984659319600323 ] } },
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah 24 Ilir", "X": 472231.43, "Y": 9670099.32 }, "geometry": { "type": "Point", "coordinates": [ 104.750114962434267, -2.984659319600323 ] } },
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah  26 Ilir", "X": 472159.16, "Y": 9669865.66 }, "geometry": { "type": "Point", "coordinates": [ 104.749464141232835, -2.986773088311812 ] } },
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah 23 Ilir", "X": 472777.14, "Y": 9669736.57 }, "geometry": { "type": "Point", "coordinates": [ 104.755024964119826, -2.987942223129068 ] } },
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah 19 Ilir", "X": 473055.53, "Y": 9669512.97 }, "geometry": { "type": "Point", "coordinates": [ 104.757529700942513, -2.989965687455792 ] } },
+{ "type": "Feature", "properties": { "Desa": "Kantor Lurah 22 Ilir", "X": 472973.66, "Y": 9669471.7 }, "geometry": { "type": "Point", "coordinates": [ 104.75679288411277, -2.990338892756573 ] } }
+]
+}
